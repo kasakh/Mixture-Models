2adda24af35d262e4891832e61adf53bf4c5872c
hu-yijie <19987199+hu-yijie@users.noreply.github.com>
2023-01-05 21:15:52 +0800

    Add files via upload
    Added mclust and mixture-of-t models

---
+++ Mixture_Models/mclust.py
@@ -0,0 +1,137 @@
+class Mclust(MM):
+    def __init__(self, data, constraint="VVV"):
+        self.constraint_set = {'EII','VII','EEI','VEI','EVI','VVI','EEE','VEE','EVE','VVE','EEV','VEV','EVV','VVV'}
+        if constraint in self.constraint_set:
+            self.constraint = constraint
+        else:
+            raise ValueError(
+                'constraint should be one of {'EII','VII','EEI','VEI','EVI','VVI','EEE','VEE','EVE','VVE','EEV','VEV','EVV','VVV'} '
+            )
+
+        self.data_checker(data)
+
+
+    def init_params(self, num_components, scale=1.0):
+        '''
+        Mclust parametrization reference: https://sites.stat.washington.edu/raftery/Research/PDF/fraley2003.pdf#page=3
+        'volumes' = log lambda (or log lambda_k)
+        'shapes' = log (diag A) (or log diag A_k) (n.b. product of diagonal elements must = 1)
+        'orientations' = Cayley parametrization of the orthogonal matrix D (or D_k) of eigenvectors: https://planetmath.org/cayleysparameterizationoforthogonalmatrices.
+        WLOG we may assume all its eigenvalues are +1 rather than -1; otherwise we could just multiply -1 to each eigenvector row/column (by multiplying with the appropriate elementary matrix E, once on the left and once on the right, i.e. DAD^T |-> D'EAED'^T = D'AD'^T).
+        '''
+        self.num_clust_checker(num_components)
+        D = self.num_dim
+        return_dict = {}
+        return_dict["log proportions"] = np.random.randn(num_components) * scale
+        return_dict["means"] = np.random.randn(num_components, D) * scale
+        self.num_freeparam = num_components*(1+D)-1
+
+        if self.constraint[0] == 'E':
+            return_dict["volumes"] = np.random.randn(1) * scale
+            self.num_freeparam += 1
+        elif self.constraint[0] == 'V':
+            return_dict["volumes"] = np.random.randn(num_components) * scale
+            self.num_freeparam += num_components
+
+        if self.constraint[1] == 'I':
+            return_dict["shapes"] = np.random.randn(0)
+        elif self.constraint[1] == 'E':
+            return_dict["shapes"] = np.random.randn(D) * scale
+            self.num_freeparam += D-1
+        elif self.constraint[1] == 'V':
+            return_dict["shapes"] = np.random.randn(num_components, D) * scale
+            self.num_freeparam += num_components*(D-1)
+
+        if self.constraint[2] == 'I':
+            return_dict["orientations"] = np.random.randn(0)
+        elif self.constraint[2] == 'E':
+            return_dict["orientations"] = np.random.randn(D,D)
+            self.num_freeparam += D*(D-1)*0.5
+        elif self.constraint[2] == 'V':
+            return_dict["orientations"] = np.random.randn(num_components,D,D)
+            self.num_freeparam += num_components*D*(D-1)*0.5
+        
+        return return_dict
+    
+
+    def unpack_params(self, params):
+        normalized_log_proportions = self.log_normalize(params["log proportions"])
+
+        num_components = len(normalized_log_proportions)
+        D = self.num_dim
+
+        if self.constraint[0] == 'E':
+            volumes = np.exp(return_dict["volumes"].repeat(num_components))
+        elif self.constraint[0] == 'V':
+            volumes = np.exp(return_dict["volumes"])
+
+        if self.constraint[1] == 'I':
+            shapes = np.zeros((num_components,D,D)) + np.eye(D)
+        elif self.constraint[1] == 'E':
+            shapes = np.zeros((num_components,D,D)) + np.diag(np.exp(return_dict["shapes"]-np.sum(return_dict["shapes"])/D))
+        elif self.constraint[1] == 'V':
+            shapes = np.apply_along_axis(np.diag,-1,np.exp(return_dict["shapes"]-np.sum(return_dict["shapes"])/D))
+
+        if self.constraint[2] == 'I':
+            orientations = np.zeros((num_components,D,D)) + np.eye(D)
+        elif self.constraint[2] == 'E':
+            skew_symm = (return_dict["orientations"]-np.transpose(return_dict["orientations"]))/2
+            orientations = np.zeros((num_components,D,D)) + ((np.eye(D) + skew_symm) @ np.linalg.inv(np.eye(D) - skew_symm))
+        elif self.constraint[2] == 'V':
+            skew_symm = (return_dict["orientations"]-np.transpose(return_dict["orientations"],(0,2,1)))/2
+            orientations = (np.eye(D) + skew_symm) @ np.linalg.inv(np.eye(D) - skew_symm)
+
+        return normalized_log_proportions, params["means"], shapes, orientations, volumes
+
+
+
+    def mclust_likelihood(self, params):
+        "returns the likelihood of mclust"
+        cluster_lls = []
+        for log_proportion, mean, shape, orientation, volume in zip(*self.unpack_params(params)):
+            cov = volume * orientation @ shape @ orientation.T
+            cluster_lls.append(log_proportion + mvn.logpdf(self.data, mean, cov))
+
+        return np.sum(logsumexp(np.vstack(cluster_lls),axis=0))
+
+    def objective(self, params):
+        return -self.mclust_likelihood(params)
+
+
+     def aic(self, params):
+        return 2 * self.num_freeparam + 2 * self.objective(params)
+
+    def bic(self, params):
+        return np.log(
+            self.num_datapoints
+        ) * self.num_freeparam + 2 * self.objective(params)
+
+
+
+    def labels(self, data, params):
+        cluster_lls = []
+        for log_proportion, mean, shape, orientation, volume in zip(*self.unpack_params(params)):
+            cov = volume * orientation @ shape @ orientation.T
+            cluster_lls.append(log_proportion + mvn.logpdf(self.data, mean, cov))
+
+        return np.argmax(np.array(cluster_lls).T,axis=1)
+
+
+    def fit(self, init_params, opt_routine, draw=None, **kargs):
+
+        self.params_store = []
+        flattened_obj, unflatten, flattened_init_params = flatten_func(
+            self.objective, init_params
+        )
+
+        def callback(flattened_params):
+            params = unflatten(flattened_params)
+            print("Log likelihood {}".format(self.likelihood(params)))
+
+            #
+            self.params_store.append(params)
+
+        self.optimize(
+            flattened_obj, flattened_init_params, callback, opt_routine, **kargs
+        )
+        return self.params_store  
---
+++ Mixture_Models/mixt.py
@@ -0,0 +1,57 @@
+from .mixture_models import *
+#import scipy.stats.multivariate_t as mvt
+from scipy.special import gammaln
+
+class TMM(GMM):
+    def init_params(self, num_components, scale=1.0):
+        self.num_clust_checker(num_components)
+        D = self.num_dim
+        
+        self.num_freeparam = num_components * (1+D+0.5*D*(D+1)+1)-1
+
+        return {
+            "log proportions": np.random.randn(num_components) * scale,
+            "means": np.random.randn(num_components, D) * scale,
+            "sqrt_covs": np.zeros((num_components, D, D)) + np.eye(D),
+            "log_dofs": np.random.randn(num_components),
+            
+        }
+
+
+    def unpack_params(self, params):
+        normalized_log_proportions = self.log_normalize(params["log proportions"])
+        dofs = np.exp(params["log_dofs"])
+        return normalized_log_proportions, params["means"], params["sqrt_covs"], dofs
+
+    def likelihood(self, params):
+        cluster_lls = []
+        for log_proportion, mean, cov_sqrt, dof in zip(*self.unpack_params(params)):
+            #logpdf = mvt.logpdf(self.data, mean, cov_sqrt.T @ cov_sqrt, dof)
+            D = mean.shape[1]
+            logpdf = gammaln((dof+D)/2)-gammaln(dof/2)-0.5*np.log(np.linalg.det(np.pi*dof*cov_sqrt.T @ cov_sqrt)) - 0.5*(dof+D)*np.log(1+np.sum(((self.data-mean)@np.linalg.inv(cov_sqrt))**2,axis=1)/dof)
+            cluster_lls.append(log_proportion + logpdf)
+        return np.sum(logsumexp(np.vstack(cluster_lls),axis=0))
+
+        
+
+    def objective(self, params):
+        return -self.likelihood(params)
+
+
+    def aic(self, params):
+        return 2 * self.num_freeparam + 2 * self.objective(params)
+
+    def bic(self, params):
+        return np.log(
+            self.num_datapoints
+        ) * self.num_freeparam + 2 * self.objective(params)
+
+    def labels(self, data, params_store):
+        cluster_lls = []
+        for log_proportion, mean, cov_sqrt, dof in zip(*self.unpack_params(params_store)):
+            #logpdf = mvt.logpdf(data, mean, cov_sqrt.T @ cov_sqrt, dof)
+            D = mean.shape[1]
+            logpdf = gammaln((dof+D)/2)-gammaln(dof/2)-0.5*np.log(np.linalg.det(np.pi*dof*cov_sqrt.T @ cov_sqrt)) - 0.5*(dof+D)*np.log(1+np.sum(((data-mean)@np.linalg.inv(cov_sqrt))**2,axis=1)/dof)
+            cluster_lls.append(log_proportion + logpdf)
+        return np.argmax(np.array(cluster_lls).T,axis=1)
+        
